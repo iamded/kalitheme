c277cf99f8ba310e0825b6dfda9928d3a7922b46
iamded <dedetmasri@gmail.com>
2023-05-30 14:33:48 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+echo "Dont forget subcribe my chanel: iam ded !!!..."
+sleep 3
+termux-open-url https://youtube.com/channel/UCk0IHP3kOrqN86sXjs1c_Xw
+clear
+sleep 3
+echo "Checking package harap menunggu ya bos..!!!"
+sleep 3
+pkg update -y
+pkg upgrade -y
+pkg install bash git -y
+pkg install zsh -y
+clear
+sleep 5
+git clone https://github.com/h4ck3r0/kali-theme
+cd kalitheme
+clear
+sleep 3
+bash os.sh
